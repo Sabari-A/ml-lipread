455ca960fe8114230d68f37158e76b1c1e9f3fdf
Sabari-A <sabari.2k02@gmail.com>
2024-03-28 15:56:03 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# ml-lipread
+# Lip Reading(LipNet) using Deep Learning
+
+This project focuses on lipreading using a deep learning model developed from scratch with reference to the LipNet Model, implemented using TensorFlow. The project includes a full-stack application created with Streamlit. Below is an overview of the project components:
+
+## Installation
+
+To install the required packages, run the following command:
+```bash
+pip install -r requirements.txt
+```
+
+## Lipreading Model Development
+
+The lipreading model is developed in the `lipreading.ipynb` file. Here's a summary of the process:
+- Data Acquisition: Data is downloaded from Google Drive.
+- Data Pipeline: Creation of a data pipeline for preprocessing and feeding data into the model.
+- Model Development: Building the deep learning model.
+- Training: Training the model using the Connectionist Temporal Classification (CTC) loss function.
+- Model Optimization: To save time, pre-trained weights are imported and utilized for testing.
+
+## Full Stack Application
+
+Before running the Streamlit app, ensure to download the trained model by following the instructions provided in the `lipreading.ipynb` file.
+
+The lipreading application is implemented as a full-stack app using Streamlit. Key files include:
+- `app.py`: Main application file located in the `app` folder.
+- `modelutils.py`: Contains functions related to the model, also located in the `app` folder.
+- `utils.py`: Utility functions used throughout the program.
+
+## Usage
+
+To utilize this project, follow these steps:
+1. Clone this repository.
+2. For model creation and training, run the `lipreading.ipynb` file.
+3. Before running the Streamlit app, download the trained model as instructed in `lipreading.ipynb`.
+4. For the full-stack Streamlit app, navigate to the `app` folder and run:
+```bash
+streamlit run app.py
+```
+This will launch the full-stack app.
+
+## Demo
+
+![Quick demo of the project](demo.mov)
+
+## Acknowledgements
+
+This project utilizes the following libraries:
+- [TensorFlow](https://www.tensorflow.org/)
+- [Streamlit](https://www.streamlit.io/)
+
+## License
+
+This project is licensed under the [The Unlicense](LICENSE).
+
---
+++ app/utils.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+from typing import List
+import cv2
+import os 
+
+vocab = [x for x in "abcdefghijklmnopqrstuvwxyz'?!123456789 "]
+char_to_num = tf.keras.layers.StringLookup(vocabulary=vocab, oov_token="")
+# Mapping integers back to original characters
+num_to_char = tf.keras.layers.StringLookup(
+    vocabulary=char_to_num.get_vocabulary(), oov_token="", invert=True
+)
+
+def load_video(path:str) -> List[float]: 
+    """Loads a video from a given path.
+
+    Args:
+        path (str): The path to the video file.
+
+    Returns:
+        List[float]: A list of video frames as floats between 0 and 1.
+    """
+    #print(path)
+    cap = cv2.VideoCapture(path)
+    frames = []
+    for _ in range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))): 
+        ret, frame = cap.read()
+        frame = tf.image.rgb_to_grayscale(frame)
+        frames.append(frame[190:236,80:220,:])
+    cap.release()
+    
+    mean = tf.math.reduce_mean(frames)
+    std = tf.math.reduce_std(tf.cast(frames, tf.float32))
+    return tf.cast((frames - mean), tf.float32) / std
+    
+def load_alignments(path:str) -> List[str]: 
+    """Loads text file containing word alignments.
+
+    Args:
+        path (str): Path to the text file containing word alignments.
+
+    Returns:
+        List[str]: A list of word alignments, where each alignment is represented as a sequence of characters, where each character corresponds to a phoneme in the transcript.
+    """
+    with open(path, 'r') as f: 
+        lines = f.readlines() 
+    tokens = []
+    for line in lines:
+        line = line.split()
+        if line[2] != 'sil': 
+            tokens = [*tokens,' ',line[2]]
+    return char_to_num(tf.reshape(tf.strings.unicode_split(tokens, input_encoding='UTF-8'), (-1)))[1:]
+
+def load_data(path: str): 
+    """Loads data from the given path.
+
+    Args:
+        path (str): The path to the data file.
+
+    Returns:
+        A tuple containing the data loaded from the file.
+    """
+    path = bytes.decode(path.numpy())
+    file_name = path.split('/')[-1].split('.')[0]
+    # File name splitting for windows
+    # file_name = path.split('\\')[-1].split('.')[0]
+    video_path = os.path.join('..','data','s1',f'{file_name}.mpg')
+    alignment_path = os.path.join('..','data','alignments','s1',f'{file_name}.align')
+    frames = load_video(video_path) 
+    alignments = load_alignments(alignment_path)
+    
+    return frames, alignments
